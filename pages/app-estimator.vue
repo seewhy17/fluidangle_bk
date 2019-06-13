1d8dd345e836bdd287a48f30e0038ab07396fb77
LonelyTomb <garubav@gmail.com>
2019-06-13 16:06:39 +0100

    WIP: Design on Second Section (Platform)

--- pages/app-estimator.vue
+++ pages/app-estimator.vue
@@ -5,7 +5,7 @@
       <nav-bar />
       <section class="banner-grid">
         <div class="content">
-          <div class="title-block text-center">
+          <div class="title-block center-align">
             <h1 class="LOOKING-TO-BUILD-AN-APP">
               LOOKING TO BUILD AN APP?
             </h1>
@@ -17,10 +17,21 @@
       </section>
       <div class="blocks-container">
         <img src="/svg/Image_3.png" alt="" class="Image-3">
-        <h2 class="text-center Get-An-Estimate">
+        <h2 class="center-align Get-An-Estimate">
           Get An Estimate
         </h2>
-        <div class="content" />
+        <div class="content">
+          <section class="platform container">
+            <div class="center-align platform-title-block">
+              <h3 class="">
+                Platform
+              </h3>
+              <p>
+                Which platform will the app be built on?
+              </p>
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   </div>
@@ -38,10 +49,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .text-center {
-    text-align: center;
-  }
-
   .page {
     .banner {
       padding-top: 2rem;
@@ -71,7 +78,7 @@ export default {
           line-height: 1.2;
 
           h1 {
-            font-size: 54px;
+            font-size: 3em;
             letter-spacing: normal;
             color: #3c2a9d;
           }
@@ -87,10 +94,14 @@ export default {
       .blocks-container {
         background-image: linear-gradient(184deg, #503cbe, #4527a0);
         position: relative;
-        margin:{
-          top:40vh;
+        margin: {
+          top: 40vh;
         }
-        .Image-3{
+        padding: {
+          top: 3%;
+        };
+
+        .Image-3 {
           /*width: 888px;*/
           /*height: 520px;*/
           position: absolute;
@@ -98,19 +109,55 @@ export default {
           top: -30vh;
           margin: 0 auto;
           left: 0;
-          right:0;
+          right: 0;
         }
-        .Get-An-Estimate{
+
+        .Get-An-Estimate {
           font-size: 48px;
           font-weight: bold;
           font-style: normal;
           font-stretch: normal;
           line-height: 1.21;
           letter-spacing: normal;
-          color: #ffffff;
+          color: $light;
+          margin: {
+            bottom: 3%;
+          };
         }
-        .content{
+
+        .content {
           height: 100vh;
+
+          .platform {
+            height: 463px;
+            border-radius: 10px;
+            box-shadow: 0 3px 20px 0 rgba(81, 63, 152, 0.54);
+            background-color: #ffffff;
+            margin: 0 auto;
+            padding: 3% 4%;
+          }
+
+          .platform-title-block {
+            font-weight: normal;
+            font-style: normal;
+            font-stretch: normal;
+            letter-spacing: normal;
+
+            h3 {
+              /*width: 168px;*/
+              /*height: 54px;*/
+              line-height: 1.2;
+              font-size: 2.2em;
+              color: #4628a1;
+            }
+
+            p {
+              /*width: 552px;*/
+              /*height: 39px;*/
+              font-size: 1.8em;
+              color: #555555;
+            }
+          }
         }
       }
     }
