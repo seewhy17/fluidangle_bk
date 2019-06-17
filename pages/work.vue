4c41fed8bb439b16cdc7ac74c51baa31ce8fa1d6
LonelyTomb <garubav@gmail.com>
2019-06-17 21:50:59 +0100

    Project Section Completed asset media files imported

--- pages/work.vue
+++ pages/work.vue
@@ -29,94 +29,130 @@
           <li><a class="bold" href="">OUR PRODUCTS</a></li>
         </ul>
       </div>
-      <div class="projects-grid container flex dir-row">
+      <div class="projects-grid container grid equal-two">
         <div class="project heroshe">
-          <div class="flex dir-row">
+          <div class="grid equal-two">
             <div class="image-part">
               <img
                 src="/img/Screenshot 2019-05-01 at 6.07.27 AM copy.png"
                 srcset="img/Screenshot 2019-05-01 at 6.07.27 AM copy@2x.png 2x,
              /img/Screenshot 2019-05-01 at 6.07.27 AM copy@3x.png 3x"
                 class=""
-              alt="">
+                alt=""
+              >
             </div>
-            <div class="content">
+            <div class="content grid equal-two-rows">
               <div class="content-box">
-                <p class="title">Heroshe</p>
-                <p class="text">We ship from any US
-                  Online Store</p>
+                <p class="title">
+                  <img src="/svg/heroshe-colored_2.svg" class="" alt="heroshe">
+                </p>
+                <p class="text">
+                  We ship from any US
+                  Online Store
+                </p>
               </div>
               <div class="action-box">
                 <span class="action-word bold">View case study</span>
               </div>
             </div>
           </div>
+          <div class="cover" />
         </div>
-        <div class="project heroshe">
-          <div class="flex dir-row">
+        <div class="project plinkd">
+          <div class="grid equal-two">
             <div class="image-part">
               <img
                 src="/img/Screenshot 2019-05-01 at 6.07.27 AM copy.png"
                 srcset="img/Screenshot 2019-05-01 at 6.07.27 AM copy@2x.png 2x,
              /img/Screenshot 2019-05-01 at 6.07.27 AM copy@3x.png 3x"
                 class=""
-                alt="">
+                alt=""
+              >
             </div>
-            <div class="content">
+            <div class="content grid equal-two-rows">
               <div class="content-box">
-                <p class="title">Heroshe</p>
-                <p class="text">We ship from any US
-                  Online Store</p>
+                <p class="title">
+                  <img
+                    src="/img/LOGO text.png"
+                    srcset="/img/LOGO text@2x.png 2x,
+             /img/LOGO text@3x.png 3x"
+                    class=""
+                    alt="plinkd logo"
+                  >
+                </p>
+                <p class="text">
+                  One Location
+                  Connecting Friends mobile app
+                </p>
               </div>
               <div class="action-box">
                 <span class="action-word bold">View case study</span>
               </div>
             </div>
           </div>
+          <div class="cover" />
         </div>
-        <div class="project heroshe">
-          <div class="flex dir-row">
+        <div class="project prontocare">
+          <div class="grid equal-two">
             <div class="image-part">
               <img
                 src="/img/Screenshot 2019-05-01 at 6.07.27 AM copy.png"
                 srcset="img/Screenshot 2019-05-01 at 6.07.27 AM copy@2x.png 2x,
              /img/Screenshot 2019-05-01 at 6.07.27 AM copy@3x.png 3x"
                 class=""
-                alt="">
+                alt=""
+              >
             </div>
-            <div class="content">
+            <div class="content grid equal-two-rows">
               <div class="content-box">
-                <p class="title">Heroshe</p>
-                <p class="text">We ship from any US
-                  Online Store</p>
+                <p class="title">
+                  ProntoCare
+                </p>
+                <p class="text">
+                  We ship from any US
+                  Online Store
+                </p>
               </div>
               <div class="action-box">
                 <span class="action-word bold">View case study</span>
               </div>
             </div>
           </div>
+          <div class="cover" />
         </div>
-        <div class="project heroshe">
-          <div class="flex dir-row">
+        <div class="project ponos">
+          <div class="grid equal-two">
             <div class="image-part">
               <img
-                src="/img/Screenshot 2019-05-01 at 6.07.27 AM copy.png"
-                srcset="img/Screenshot 2019-05-01 at 6.07.27 AM copy@2x.png 2x,
-             /img/Screenshot 2019-05-01 at 6.07.27 AM copy@3x.png 3x"
+                src="/img/Image 4.png"
+                srcset="img/Image 4@2x.png 2x,
+             /img/Image 4@3x.png 3x"
                 class=""
-                alt="">
+                alt=""
+              >
             </div>
-            <div class="content">
+            <div class="content grid equal-two-rows">
               <div class="content-box">
-                <p class="title">Heroshe</p>
-                <p class="text">We ship from any US
-                  Online Store</p>
+                <p class="title">
+                  <img
+                    src="/img/Image 5.png"
+                    srcset="img/Image 5@2x.png 2x,
+             /img/Image 5@3x.png 3x"
+                    class=""
+                    alt=""
+                  >
+                </p>
+                <p class="text">
+                  Making Living & Traveling <br>
+                  More Affordable for Everyone
+                </p>
               </div>
               <div class="action-box">
                 <span class="action-word bold">View case study</span>
               </div>
             </div>
           </div>
+          <div class="cover" />
         </div>
       </div>
     </section>
@@ -156,6 +192,7 @@ export default {
         height: 100%;
         z-index: -1;
       }
+
       .banner-grid {
         padding: 4rem 5vw 0;
         overflow-x: hidden;
@@ -176,54 +213,157 @@ export default {
           line-height: 2;
         }
       }
-      .project-amount{
+
+      .project-amount {
         color: $primary;
-        margin:{top:12rem;}
-        p{
+        margin: {
+          top: 12rem;
+        }
+
+        p {
           font-size: 1.7rem;
           position: absolute;
           bottom: -15%;
           display: inline-flex;
-          padding:1.8rem 2rem;
+          padding: 1.8rem 2rem;
           background: white;
           align-items: center;
-          color:$gray;
+          color: $gray;
           border: solid 1px $gray;
         }
-        span{
+
+        span {
           font-size: 3rem;
           font-weight: 500;
-          color:$primary
+          color: $primary
         }
       }
     }
-    .projects{
+
+    .projects {
       margin-top: 8rem;
-      padding-bottom: 3rem;
-      .filter-options{
+
+      .filter-options {
         width: 50%;
         min-width: 50%;
-        li{
+
+        li {
           width: 25%;
           list-style-type: none;
         }
-        a{
+
+        a {
           font-size: 1rem;
           padding: {
             bottom: .5rem;
           };
         }
-        .selected{
-          color:$primary;
+
+        .selected {
+          color: $primary;
           border-bottom: solid 2px $primary;
         }
       }
-      .projects-grid{
+
+      .projects-grid {
         margin-top: 3rem;
-        .project{
-          width: 50%;
+        padding: {
+          top: 5rem;
+          bottom: 3rem;
+        }
+        grid-gap: 5em 1em;
+        @include breakpoint ($max: 768px) {
+          grid-template-columns: 1fr;
+        }
+
+        .project {
+          height: 17rem;
+          background-color: $gray;
+          margin-bottom: 3rem;
+          position: relative;
+
+          &.heroshe {
+            background-color: #47b6d2;
+            img{
+              width: 10rem;
+            }
+          }
+
+          &.ponos {
+            background-color: $gray;
+            img{
+              width: 9.83rem;
+            }
+          }
+
+          &.plinkd {
+            background-color: #f9872b;
+            img{
+              width:9.5rem;
+            }
+          }
+
+          &.prontocare {
+            background-color: #0254a3;
+          }
+
+          .equal-two {
+            height: 100%;
+          }
+
+          .image-part {
+            position: relative;
+            padding: {
+              left: 3rem;
+            };
+
+            img {
+              width: 70%;
+              position: absolute;
+              top: -20%;
+            }
+          }
+
+          .content-box {
+            justify-content: space-between;
+            padding-top: 3rem;
+
+            .title {
+              color:$light;
+              font-weight: 700;
+              font-size: 1.8rem;
+            }
+
+            .text {
+              font-size: .8rem;
+              color:$light;
+              font-weight: 600;
+            }
+          }
+          .action-word {
+            text-transform: uppercase;
+            font-size: .8rem;
+            margin-top: 1.5rem;
+            display: block;
+            color: $light;
+
+            &:after {
+              content: "\02192";
+              font-size: 1.5rem;
+              font-weight: 700;
+            }
+          }
+        }
+
+        .cover {
+          height: 4rem;
+          bottom: -4rem;
+          position: absolute;
+          width: 100%;
+          background-color: #fff;
+          /*top: 100%;*/
         }
       }
     }
-    }
+  }
 </style>
