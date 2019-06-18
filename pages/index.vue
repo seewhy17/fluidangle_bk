eea6222435599b007d1717d9b6ac05d221a70885
LonelyTomb <garubav@gmail.com>
2019-06-18 23:41:33 +0100

    Added Components start-project & statistics Removed sections start-project & statistics

--- pages/index.vue
+++ pages/index.vue
@@ -144,122 +144,8 @@
       </div>
     </section>
 
-    <section class="section our-statistics">
-      <div class="container flex dir-row statistics-grid">
-        <div class="stats grid equal-two-rows">
-          <div class="image-part">
-            <img
-              src="/img/stats-clients-b5bbbd87.png"
-              srcset="/img/stats-clients-b5bbbd87@2x.png 2x,
-             /img/stats-clients-b5bbbd87@3x.png 3x"
-              alt="stats-clients"
-            >
-          </div>
-          <div class="content">
-            <p class="amount">
-              60+
-            </p>
-            <p class="comment">
-              HAPPY CLIENTS
-            </p>
-          </div>
-        </div>
-        <div class="stats grid equal-two-rows">
-          <div class="image-part">
-            <img
-              src="/img/stats-commits-5a037d2c.png"
-              srcset="/img/stats-commits-5a037d2c@2x.png 2x,
-             /img/stats-commits-5a037d2c@3x.png 3x"
-              alt="stats-commits"
-            >
-          </div>
-          <div class="content">
-            <p class="amount">
-              90+
-            </p>
-            <p class="comment">
-              SUCCESSFUL PROJECT
-            </p>
-          </div>
-        </div>
-        <div class="stats grid equal-two-rows">
-          <div class="image-part">
-            <img
-              src="/img/stats-projects-efbd5c8b.png"
-              srcset="/img/stats-projects-efbd5c8b@2x.png 2x,
-             /img/stats-projects-efbd5c8b@3x.png 3x"
-              alt="stats-projects"
-            >
-          </div>
-          <div class="content">
-            <p class="amount">
-              63+
-            </p>
-            <p class="comment">
-              ACCEPTED USER STORIES
-            </p>
-          </div>
-        </div>
-        <div class="stats grid equal-two-rows">
-          <div class="image-part">
-            <img
-              src="/img/stats-user-stories-a8e2c535.png"
-              srcset="/img/stats-user-stories-a8e2c535@2x.png 2x,
-             /img/stats-user-stories-a8e2c535@3x.png 3x"
-              alt="stats-user-stories"
-            >
-          </div>
-          <div class="content">
-            <p class="amount">
-              339+
-            </p>
-            <p class="comment">
-              DELIVERED GIT COMMITS
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="container grid works-grid">
-        <div>
-          <p class="content-text">
-            People that trust our work
-          </p>
-        </div>
-        <div>
-          <img src="/svg/heroshe-colored.svg" class="partner-logo" alt="heroshe">
-        </div>
-        <div>
-          <img
-            src="/img/Screenshot 2019-05-01 at 6.25.48 AM.png"
-            srcset="/img/Screenshot 2019-05-01 at 6.25.48 AM.@2x.png 2x,
-             /img/Screenshot 2019-05-01 at 6.25.48 AM.@3x.png 3x"
-            alt="proto"
-          >
-        </div>
-        <div>
-          <img
-            src="/img/Image 7.png"
-            srcset="/img/Image 7@2x.png 2x,
-             /img/Image 7@3x.png 3x"
-            alt="ponos"
-          >
-        </div>
-      </div>
-      <div class="clients container" />
-    </section>
-    <section class="section start-project">
-      <div class="content">
-        <p class="title">
-          Ready to start a project?
-        </p>
-        <div class="">
-          <p class="text">
-            Let’s build something awesome together! <br>Say contact@fluidangle.com or call: 234803823231
-          </p>
-        </div>
-        <button>Start Project</button>
-      </div>
-    </section>
+    <statistics />
+    <start-project />
     <Footer />
   </div>
 </template>
@@ -270,7 +156,9 @@ export default {
   name: 'Index',
   components: {
     NavBar,
-    Footer
+    Footer,
+    'Statistics': () => ({ component: import('~/components/statistics.vue') }),
+    'StartProject': () => ({ component: import('~/components/startProject.vue') })
   }
 }
 </script>
@@ -549,86 +437,6 @@ export default {
             height: 30rem;
             object-fit: cover;
           }
-        }
-      }
-
-      &.our-statistics {
-        margin-top: 5rem;
-        padding-bottom: 3rem;
-
-        .statistics-grid {
-          min-width: 65%;
-          width: 65%;
-          align-items: center;
-        }
-
-        .stats {
-          width: 25%;
-          justify-items: center;
-          text-align: center;
-
-          &:nth-child(n+2) {
-            border-left: solid 2px $light-purple;
-          }
-
-          .amount {
-            font-size: 3.2rem;
-            font-weight: 600;
-            color: $dark-grey;
-          }
-
-          .comment {
-            font-size: .8rem;
-            color: $light-gray;
-            font-weight: 600;
-          }
-        }
-
-        .works-grid {
-          margin: {
-            top: 5rem;
-          }
-          grid-template-columns: repeat(4, 1fr);
-          align-items: center;
-
-          .content-text {
-            font-size: 1rem;
-          }
-        }
-
-        .clients {
-          min-width: 65%;
-          width: 65%;
-        }
-      }
-
-      &.start-project {
-        padding: 4.8rem 9rem;
-        background-color: $primary;
-        color: $light;
-
-        .title {
-          font-size: 1.8rem;
-          margin: {
-            bottom: 1.5rem;
-          };
-        }
-
-        .text {
-          font-size: 0.95rem;
-        }
-
-        button {
-          height: 3rem;
-          width: 8rem;
-          margin: {
-            top: 1.5rem;
-          };
-          border-style: none;
-          border-radius: 1.7rem;
-          color: $primary;
-          background-color: $light;
-          font-size: .95rem;
         }
       }
     }
