f5552f0348b4f67b6784f11d437cc729e696aeaf
LonelyTomb <garubav@gmail.com>
2019-06-21 20:27:54 +0100

    WIP: Trending Section

--- pages/blog.vue
+++ pages/blog.vue
@@ -32,7 +32,7 @@
     </div>
     <section class="blog-container container">
       <div class="posts-container grid">
-        <section class="posts">
+        <section class="post-main">
           <div class="post card main-post">
             <div class="image-part">
               <img src="/img/custom/General (24).jpg" alt="">
@@ -80,6 +80,78 @@
           </div>
         </aside>
       </div>
+      <div class="posts-container grid more-posts">
+        <div class="wrapper">
+          <div class="posts grid equal-two">
+            <div v-for="post in posts" :key="post.id" class="post">
+              <div class="image-part">
+                <img :src="post.image" alt="">
+              </div>
+              <div class="content-box">
+                <div class="content">
+                  <h3 class="title">
+                    {{ post.title }}
+                  </h3>
+                  <p class="text">
+                    {{ post.description }}
+                  </p>
+                </div>
+                <div class="action-box flex dir-row">
+                  <a href="">{{ post.type }}</a>
+                  <p>{{ post.minutes }} mins read</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="most-popular">
+            <div class="header">
+              <h2 class="title">
+                MOST POPULAR
+              </h2>
+            </div>
+            <div v-for="popular in mostPopular" :key="popular.key" :id="popular.key" class="content-box">
+              <div class="content">
+                <h3 class="title">
+                  {{ popular.title }}
+                </h3>
+                <p class="text">
+                  {{ popular.description }}
+                </p>
+              </div>
+              <div class="action-box flex dir-row">
+                <a href="">{{ popular.type }}</a>
+                <p>{{ popular.minutes }} mins read</p>
+              </div>
+            </div>
+          </div>
+          <div class="wrapper trending">
+            <div class="header">
+              <h2 class="title">
+                TRENDING
+              </h2>
+            </div>
+            <div class="posts grid equal-two">
+              <div v-for="post in posts" :key="post.id" class="post">
+                <div class="image-part">
+                  <img :src="post.image" alt="">
+                </div>
+                <div class="content-box">
+                  <div class="content">
+                    <h3 class="title">
+                      {{ post.title }}
+                    </h3>
+                  </div>
+                  <div class="action-box flex dir-row">
+                    <a href="">{{ post.type }}</a>
+                    <p>{{ post.minutes }} mins read</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <aside />
+      </div>
     </section>
     <Footer />
   </div>
@@ -93,11 +165,42 @@ export default {
   components: {
     NavBar,
     Footer
-  }
+  },
+  data: () => ({
+    posts: [
+      {
+        id: 'post01',
+        title: 'The 3 Things Women Over 50 Can Do to Keep Their Jobs',
+        description: 'Workers will no longer tolerate the punishing schedules of technology giants Workers will no longer tolerate the punishing schedules of technology giants',
+        type: 'Development',
+        minutes: '7',
+        image: '/img/custom/General (19).jpg'
+      },
+      {
+        id: 'post01',
+        title: 'The 3 Things Women Over 50 Can Do to Keep Their Jobs',
+        description: 'Workers will no longer tolerate the punishing schedules of technology giants Workers will no longer tolerate the punishing schedules of technology giants',
+        type: 'Development',
+        minutes: '7',
+        image: '/img/custom/General (19).jpg'
+      }
+    ],
+    mostPopular: [
+      {
+        id: 'popular-01',
+        title: 'Want to Retire Rich? Don’t Make This Common Mistake.',
+        type: 'Design',
+        minutes: '6'
+      }
+    ]
+  })
 }
 </script>
 
 <style scoped lang="scss">
+  body{
+    background-color: #fdfdfd;;
+  }
   .page {
     .banner {
       padding-top: 2rem;
@@ -203,17 +306,25 @@ export default {
         right: 6%;
         left: 9%;
       }
-      .card{
+
+      .card {
         border-radius: 20px;
         box-shadow: 0 3px 20px 0 rgba(0, 0, 0, 0.1);
         padding: {
           bottom: 2rem;
         };
       }
-      .posts-container{
-      grid-template-columns: 2fr 1fr;
-      grid-gap: 2.5rem;
-}
+
+      .posts-container {
+        grid-template-columns: 2fr 1fr;
+        grid-gap: 2.5rem;
+        margin-bottom: 3rem;
+      }
+
+      .posts {
+        margin-bottom: 10%;
+      }
+
       .post {
         height: 100%;
         display: grid;
@@ -257,9 +368,10 @@ export default {
 
           .action-box {
             color: $primary;
+            align-items: center;
             a {
               padding-right: 1.5rem;
-              border-right: solid 2px purple;
+              border-right: solid 2px $deep-purple;
               margin-right: 1rem;
             }
           }
@@ -268,12 +380,14 @@ export default {
 
       .sub-post {
         border-radius: 12px;
-        .image-part{
-          img{
+
+        .image-part {
+          img {
             border-top-left-radius: 12px;
             border-top-right-radius: 12px;
           }
         }
+
         .content-box {
           padding: {
             top: 1rem;
@@ -299,6 +413,7 @@ export default {
           }
 
           .action-box {
+            align-items: center;
             a {
               border: {
                 right: 0;
@@ -306,6 +421,74 @@ export default {
               padding-right: 0;
             }
           }
+        }
+      }
+
+      .more-posts {
+        .posts {
+          grid-gap: 2rem;
+        }
+
+        .post {
+          align-content: space-evenly;
+
+          .image-part {
+            img {
+              border-radius: 0;
+              height: 18.75rem;
+            }
+          }
+
+          .content-box {
+            padding: {
+              top: 1rem;
+              left: 0;
+            }
+
+            .content {
+              width: 100%;
+            }
+
+            h3 {
+              font-weight: 500;
+              width: 85%;
+            }
+
+            p {
+              width: 80%;
+              font-size: .9rem;
+            }
+          }
+        }
+      }
+      .header{
+        margin-bottom: 2rem;
+        .title{
+          color:$deep-purple;
+          font-weight: 500;
+          line-height: 1.61;
+          padding-bottom: .5rem;
+          border-bottom: solid 2px $light-gray;
+        }
+      }
+      .most-popular{
+        .content-box{
+          h3{
+            font-weight: 500;
+            line-height: 1.61;
+            color:$gray;
+            font-size: 1.5rem;
+            margin-bottom: 1.5rem;
+          }
+        }
+        .action-box{
+            color: $gray;
+          align-items: center;
+            a {
+              padding-right: 1.5rem;
+              border-right: solid 2px $gray;
+              margin-right: 1rem;
+            }
         }
       }
     }
