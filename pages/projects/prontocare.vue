912630cdf4e5c6cb766ab49f08817c321f2ef6c0
LonelyTomb <garubav@gmail.com>
2019-06-22 16:48:53 +0100

    Base Layouts For The Project Pages

---
+++ pages/projects/prontocare.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page">
+    <div class="banner">
+      <span class="bg" />
+      <nav-bar />
+      <section class="banner-grid">
+        <div class="content">
+          <div class="title-block center-align">
+            <h1 class="title">
+              <img src="/img/Image 15.png" alt="">
+            </h1>
+            <div class="image-part">
+              <img src="" alt="">
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="blocks-container" />
+    </div>
+    <start-project />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/partials/navBar.vue'
+import Footer from '~/components/partials/Footer.vue'
+
+export default {
+  name: 'ProntoCare',
+  NavBar,
+  Footer,
+  'StartProject': () => ({ component: import('~/components/startProject.vue') })
+}
+</script>
+
+<style scoped>
+
+</style>
